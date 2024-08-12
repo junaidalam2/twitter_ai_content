439cb60d445bfb72ed1290d4ab5b2329f5460977
junaidalam2 <junaid.alam2@gmail.com>
2024-08-12 13:05:08 -0700

    added more senstive topics in utils and completed readme

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
 TWITTER_CHARACTER_LIMIT = 280
 BUFFER_TWITTER_CHAR_LIMIT = 33
-SENSITIVE_WORD_ARRAY = ['war', 'abuse' , 'death' , 'serious injury']
+SENSITIVE_WORD_ARRAY = ['war', 'abuse' , 'death' , 'serious injury', 'abortion', 'capital punishment', 'rape', 'child abuse', 'disabililty', 'car crash', 'train crash', 'plane crash', 'illness']
